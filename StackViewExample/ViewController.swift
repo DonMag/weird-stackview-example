7689a38e66ed5d094d5afd629e14b7b1d3d49109
0xSkash <48493983+SkashEU@users.noreply.github.com>
2024-02-12 17:23:05 +0100

    created project

---
+++ StackViewExample/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  StackViewExample
+//
+//  Created by Skash on 12.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var exampleButton: UIButton!
+    @IBOutlet var exampleButtonInStackView: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .black
+
+        exampleButtonInStackView.titleLabel?.adjustsFontSizeToFitWidth = true
+        exampleButtonInStackView.titleLabel?.adjustsFontSizeToFitWidth = true
+        exampleButtonInStackView.layer.cornerRadius = 30
+        exampleButtonInStackView.backgroundColor = UIColor.white
+
+        exampleButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        exampleButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        exampleButton.layer.cornerRadius = 30
+        exampleButton.backgroundColor = UIColor.white
+
+        let insets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        exampleButtonInStackView.contentEdgeInsets = insets
+        exampleButton.contentEdgeInsets = insets
+    }
+}
